f35ae92de9bf69495dcf80b483d409ade567200a
Olga Cortez <olgalataviacortez@gmail.com>
2019-10-31 14:11:07 -0500

    testing

--- index.js
+++ index.js
@@ -8,6 +8,24 @@ const server = express();
 
 server.use(express.json());
 
+const router = require("express").Router();
+
+const Post = require("./db.js");
+
+//GETS ALL POSTS
+router.get("/", (req, res) => {
+    Post.find()
+      .then(posts => {
+        res.status(200).json(posts);
+      })
+      .catch(err => {
+        console.log("error", err);
+        res
+          .status(500)
+          .json({ error: "The posts information could not be retireved." });
+      });
+  });
+
 server.use('/api/posts', postRouter);
 
 const port = process.env.PORT;
@@ -15,3 +33,5 @@ const port = process.env.PORT;
 server.listen(port, () => {
     console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+module.exports = router;
